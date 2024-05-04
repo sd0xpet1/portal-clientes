8605eeef256d08456b65c14b0773211720facb1d
wardifyapp <wardifyapp@gmail.com>
2024-05-04 17:53:31 -0400

    update button
    update button

--- modelo/segments/btn-generar-reporte.php
+++ modelo/segments/btn-generar-reporte.php
@@ -1,4 +1,4 @@
-
-                    <!-- Boton Generar Reporte   -->
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i>Generar Reporte</a>
+  <!-- Boton Generar Reporte   -->
+ <button type="button" class="btn d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mb-3" onclick="window.location.href = '#';">
+   <i class="fas fa-download fa-sm text-white-50"></i> Generar Reporte
+</button>
--- modelo/segments/registro-miper.php
+++ modelo/segments/registro-miper.php
@@ -2,3 +2,4 @@
 <button type="button" class="btn btn-danger d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mb-3" onclick="window.location.href = 'registrar-miper.php';">
    <i class="fas fa-download fa-sm text-white-50"></i> Registrar Nueva Actividad a MIPER+C
 </button>
+<?php include 'btn-generar-reporte.php';?>
