e639d8f51ca381ba62132b4a0461f3696f0e5c54
CG <kevburgosr@gmail.com>
2024-05-04 17:47:40 -0400

    ..
    ..

---
+++ modelo/segments/btn-generar-reporte.php
@@ -0,0 +1,4 @@
+
+                    <!-- Boton Generar Reporte   -->
+                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i>Generar Reporte</a>
--- modelo/segments/content-empresas.php
+++ modelo/segments/content-empresas.php
@@ -8,8 +8,8 @@ include 'registro-empresa.php';
                         <div class="col-lg-6 mb-4">
                             <div class="card bg-white text-black shadow">
                                 <div class="card-body">
-                                    CFT Lota Arauco
-                                    <div class="text-black-50 small">Asesor a cargo: Luis Sliming</div>
+                                    Krash+
+                                    <div class="text-black-50 small">Asesor a cargo: Kevin Burgos</div>
                                 </div>
                             </div>
                         </div>
